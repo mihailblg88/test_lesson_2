dbc9737491344158a747a36ec3a31fe9f31a5864
mihailblg88 <mikhafilippov88@gmail.com>
2023-02-23 15:23:58 -0500

    Добавили еще строку

---
+++ test_simple.py
@@ -0,0 +1,28 @@
+import time
+
+import pytest
+from selene import browser
+from selene import be, have
+
+
+@pytest.fixture()
+def browser_size():
+    browser.config.window_height = 720
+    browser.config.window_width = 1280
+    return browser
+
+
+@pytest.fixture()
+def browser_open(browser_size):
+    browser.open('https://google.com/')
+
+
+def test_web_search_positiv(browser_open):
+    browser.element('[name="q"]').should(be.blank).type('yashaka\selene').press_enter()
+
+
+def test_web_search_negative(browser_open):
+    browser.element('[name="q"]').should(be.blank).type('775855IIIIIpp!!!').press_enter()
+    browser.element('[id="topstuff"]').should(have.text('По запросу 775855IIIIIpp!!! ничего не найдено.'))
+
+
