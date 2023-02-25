11b7925e346e6c8550c36f3b9387ca3e8c5ec757
mihailblg88 <mikhafilippov88@gmail.com>
2023-02-24 20:58:52 -0500

    Добавили еще строку

--- test_simple.py
+++ test_simple.py
@@ -1,15 +1,12 @@
-import time
-
 import pytest
-from selene import browser
 from selene import be, have
+from selene import browser
 
 
 @pytest.fixture()
 def browser_size():
     browser.config.window_height = 720
     browser.config.window_width = 1280
-
 
 
 @pytest.fixture()
@@ -24,5 +21,3 @@ def test_web_search_positiv(browser_open):
 def test_web_search_negative(browser_open):
     browser.element('[name="q"]').should(be.blank).type('775855IIIIIpp!!!').press_enter()
     browser.element('[id="topstuff"]').should(have.text('По запросу 775855IIIIIpp!!! ничего не найдено.'))
-
-
